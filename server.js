ed43537b6348fc6c5700e2fc6a27cdd2cb6831b5
Bryan Hernandez <bryanh24@hotmail.com>
2020-07-28 21:07:21 -0600

    Siendo romantico

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
     res.render("home", {
-        name: "bryan hernandez",
+        name: "Mi amor",
     });
 });
 app.get("/about", (req, res) => {
